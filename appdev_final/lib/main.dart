45759f67e9e3ed15a50354f56b53c79698cc4869
Anfa Majid <110888247+anfa-majid@users.noreply.github.com>
2025-05-29 21:09:24 +0500

    final

--- appdev_final/lib/main.dart
+++ appdev_final/lib/main.dart
@@ -1,33 +1,51 @@
-// import 'package:app_dev_final/addData.dart';
-import 'package:appdev_final/screens/product_detail_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'screens/home_screen.dart';
-import 'firebase_options.dart';
+// // import 'package:app_dev_final/addData.dart';
+// import 'package:appdev_final/screens/product_detail_screen.dart';
+// import 'package:flutter/material.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'screens/home_screen.dart';
+// import 'firebase_options.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(MyApp());
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Shopping App',
+//       debugShowCheckedModeBanner: false,
+//       routes: {
+//         '/home': (_) => HomeScreen(),
+//         '/cards': (_) => CardsScreen(),
+//       },
+//       theme: ThemeData(
+//         primarySwatch: Colors.pink,
+//         useMaterial3: true,
+//       ),
+//       home: HomeScreen(),
+//     );
+//   }
+// }
+
+import 'package:flutter/material.dart';
+import 'screens/product_detail_screen.dart'; 
+
+void main() {
+  runApp(CardsApp());
 }
 
-class MyApp extends StatelessWidget {
+class CardsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Shopping App',
+      title: 'Cards Screen Demo',
       debugShowCheckedModeBanner: false,
-      routes: {
-        '/home': (_) => HomeScreen(),
-        '/cards': (_) => CardsScreen(),
-      },
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-        useMaterial3: true,
-      ),
-      home: HomeScreen(),
+      home: CardsScreen(),
     );
   }
 }
